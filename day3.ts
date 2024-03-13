1d6e7b045502144d57a5d9859d42292490cd8a9c
javeri-a <160253455+javeri-a@users.noreply.github.com>
2024-03-13 13:36:05 +0500

    Add files via upload

---
+++ day3.ts
@@ -0,0 +1,19 @@
+//   QUESTION NUMBER 7 AND 8
+//Question 7 and 8:Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.//
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(4 + 4);
+console.log(16 / 2);
+console.log("\n");
+
+// Question 8: repeated!
+console.log(12-4);
+console.log("\n");
+
+
+//    QUESTION NUMBER 9
+//Question 9:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favoriteNumber = ("9");
+let message = ("Iwas reveal my favorite number:");
+console.log(" ".concat(message, favoriteNumber));
+console.log("\n");
